4a0f0299ea0fd959452dbc44b0369e33d962728c
girijashankar7894 <girijashankar7894@gmail.com>
2026-06-22 13:26:26 +0530

    function and scope

--- function.js
+++ function.js
@@ -41,7 +41,7 @@ function checkUsername(username) {
     // process-1
     // if(username === undefined || username === null || username === "")
 
-    // process-2
+    // process-2 : undefined = null = "" = false
     if(!username)
         return `Please enter a username`;
     else
@@ -49,3 +49,36 @@ function checkUsername(username) {
 }
 console.log(checkUsername(undefined));
 
+
+// rest(...) operator
+function funRestOperator(...value) {
+    console.log(`All Product Price = ${value}`); // All Product Price = [10,20,30,40,50]
+}
+funRestOperator(10, 20, 30, 40, 50);
+
+// rest(...) operator with some parameters
+function funRestOperator(value1, value2, ...value3) {
+    console.log(`Apple Price = ${value1}`); // Apple Price = 10 
+    console.log(`Orange Price = ${value2}`); // Orange Price = 20
+    console.log(`Other Fruits Price = ${value3}`); // Other Fruits Price = [30,40,50]
+}
+funRestOperator(10, 20, 30, 40, 50);
+
+
+// pass an array as a function argument
+function funPassArray(arr) {
+    console.log(`arr = ${arr}`); // arr = [5,15,25,35,45]
+    console.log(`arr[2] = ${arr[2]}`); // arr[2] = 25
+}
+funPassArray([5, 15, 25, 35, 45]);
+
+
+// pass an object as a function argument
+function funPassObject(obj) {
+    console.log(obj); // { productName: 'water bottle', price: 149 }
+    console.log(`obj.productName = ${obj.productName}`); // obj.productName = water bottle
+}
+funPassObject({
+    productName: "water bottle",
+    price: 149
+});
