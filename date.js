49dbdd987d8efaee957fb9bb0a7813419101703a
girijashankar7894 <girijashankar7894@gmail.com>
2026-06-22 08:22:13 +0530

    date

---
+++ date.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// create a new date object
+let date = new Date();
+console.log(date); // get the current year
+
+// create a new date object with a specific date and time
+let specificDate = new Date("2024-06-01T12:00:00");
+console.log(specificDate); // get the year, month, and day from the specific date
+
+// 
