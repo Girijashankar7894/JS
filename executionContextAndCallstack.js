411835358076a417b859d5116e237f79b94b6dd0
girijashankar7894 <girijashankar7894@gmail.com>
2026-06-26 06:22:06 +0530

    iife & execution context and callstack

---
+++ executionContextAndCallstack.js
@@ -0,0 +1,17 @@
+// JavaScript is a single threaded programming language.
+
+// Execution context
+// Basically execution context are 3 types:
+// 1. Global Execution Context
+// 2. Functional Execution Context
+// 3. Eval Execution Context
+
+
+// How javaScript code executed?
+// Basically JS codes are executed in 3 phases:
+// 1) Global Execution Phase
+// 2) Memory Creation Phase / Creation Phase
+// 3) Execution Phase
+
+
+// Callstack
