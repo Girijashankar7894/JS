2744560caabc7099ec36ddb6fb62aaf7e1604e75
girijashankar7894 <girijashankar7894@gmail.com>
2026-07-13 08:27:15 +0530

    Event Listener

---
+++ Events In JS/event.js
@@ -0,0 +1,23 @@
+// add event listener
+const gallery = document.getElementById("gallery");
+const image2 = document.getElementById("image2");
+console.log(image2);
+
+
+// Process-02:
+// image2.onclick(alert('Add event listener by using onclick function inside the external JS file'));
+
+// Process-03:
+// image2.addEventListener("click", (el) => {
+//     alert(`Add event listener using addEventListener() function.`);
+// }, false);
+
+// more about addEventListener().
+// gallery.addEventListener("click", (el) => {
+//     console.log(el);
+//     console.log(el.target);
+// });
+
+
+// event bubbling
+// gallery.addEventListener("click", (el) => {});
