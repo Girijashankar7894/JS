f91ea80e1f713cdece10151a01187dc06092aa53
girijashankar7894 <girijashankar7894@gmail.com>
2026-07-21 07:25:29 +0530

    Random Background Color

---
+++ Project6-Change Random Background Color/script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const body = document.body;
+
+// generate rgb color
+// const color = () => Math.floor(Math.random()*255);
+
+// generate hex(#) color code
+const randomColor = () => {
+    const hex = "0123456789ABCDEF";
+    let hexCode = "#";
+    for (let i = 0; i < 6; i++) {
+        hexCode += hex[Math.floor(Math.random()*16)];
+    }
+    return hexCode;
+};
+
+setInterval(() => {
+    // rgb color
+    // let backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+    // body.style.background = backgroundColor;
+        
+    body.style.background = randomColor();
+}, 2000);
